d5a7b69f0d46df94a179947633bf65707a4ef677
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-13 14:57:54 -0600

    Finished create post functioanlity and try to fix layout

--- client/src/compontents/Form/Form.js
+++ client/src/compontents/Form/Form.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
+import { useDispatch } from "react-redux";
 import useStyles from "./styles.js"
+import { createPosts } from "../../action/posts";
 
 const Form = () => {
     const [postData, setPostData] = useState({creator: "", title: "", message: "", tags: "", selectedFile: "" });
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        dispatch(createPosts(postData));
     }
 
     const clear = () => {
@@ -25,8 +30,8 @@ const Form = () => {
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth value={ postData.tags } onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
                 <div className={classes.fileInput}>
                     <FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})} />
-                    <Button className={classes.buttonSubmit} variant={"contained"} color={"primary"} size={"large"} type={"submit"} fullWidth>Submit</Button>
-                    <Button variant={"contained"} color={"secondary"} size={"small"} onClick={ clear } fullWidth>Clear</Button>
+                    <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                    <Button variant="contained" color="secondary" size="small" onClick={ clear } fullWidth>Clear</Button>
                 </div>
             </form>
         </Paper>
