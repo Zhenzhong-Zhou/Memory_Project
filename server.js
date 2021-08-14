499f00a935d2202e7f6c8840aaccfd5df3014c42
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-13 22:48:36 -0600

    Add greeting message/page

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/", (req, res) => {
+    res.send("Welcome to Memory Back End!");
+});
 app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 9000;
