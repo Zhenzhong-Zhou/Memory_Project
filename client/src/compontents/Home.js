c90a88d79d619ee46fce6af19ad8c5eda316dca5
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-14 14:16:53 -0600

    Add some new dependencies, restrcture frontend and make new layout

---
+++ client/src/compontents/Home.js
@@ -0,0 +1,10 @@
+// import React from "react";
+// import {AppBar, Typography} from "@material-ui/core";
+//
+// export const Home = () => {
+//     return (
+//
+//     );
+// };
+//
+// export default Home;
