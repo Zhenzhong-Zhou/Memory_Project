6f99a0c5fe23aafdaacecdc37a362bc9ac742081
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-13 23:14:55 -0600

    Add example file and env variable

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:9000/posts";
+const url = process.env.LOCAL_URL || "https://memory-timeline-project.herokuapp.com/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
