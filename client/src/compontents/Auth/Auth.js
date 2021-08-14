5fc4f55b1a93cce2007dedf27437549d6a79fcc7
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-14 14:45:45 -0600

    BUG FIXES: Input.js label={{label}} and add Auth page

--- client/src/compontents/Auth/Auth.js
+++ client/src/compontents/Auth/Auth.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Avatar, Button, Paper, Grid, Typography, Container } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
+import useStyles from "./styles";
+import Input from "./Input";
 
 export const Auth = () => {
-    const state = null;
+    const classes = useStyles();
+    const [showPassword, setShowPassword] = useState(false);
+    const isSignup = false;
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    }
+
+    const handleShowPassword = () => handleShowPassword(() => setShowPassword((preShowPassword) => !preShowPassword));
+
+
     return (
-        <div>
-            Auth
-        </div>
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avator}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant={"h5"}>{isSignup ? "Sign Up" : "Sign In"}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+                            <React.Fragment>
+                                <Input name="firstname" label="First Name" handleChange={handleChange} autoFocus half />
+                                <Input name="lastname" label="Last Name" handleChange={handleChange} half />
+                            </React.Fragment>
+                        )}
+                            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                            <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword}/>
+                        { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
+                    </Grid>
+                </form>
+            </Paper>
+
+        </Container>
     );
 };
 
