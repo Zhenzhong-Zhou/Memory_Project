0e90b97793ed6307d19da09d4cebd19f1bc4259d
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-15 23:40:25 -0600

    Add pagination frontend part and redesign home page and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import { Container} from "@material-ui/core";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import NavBar from "./compontents/NavBar/NavBar";
 import Home from "./compontents/Home/Home";
 import Auth from "./compontents/Auth/Auth";
+import PostDetails from "./compontents/PostDetails/PostDetails";
 
 const App = () => {
+    const user = JSON.parse(localStorage.getItem("profile"));
     return (
         <BrowserRouter>
-            <Container maxWidth={"lg"}>
+            <Container maxWidth={"xl"}>
                 <NavBar />
                 <Switch>
-                    <Route path="/" exact component={Home}/>
-                    <Route path="/auth" exact component={Auth}/>
+                    <Route path="/" exact component={() => <Redirect to="/posts" />} />
+                    <Route path="/posts" exact component={Home} />
+                    <Route path="/posts/search" exact component={Home} />
+                    <Route path="/posts/:id" component={PostDetails} />
+                    <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts/" />)} />
                 </Switch>
             </Container>
         </BrowserRouter>
     );
-}
+};
 
 export default App;
--- client/src/compontents/Home/Home.js
+++ client/src/compontents/Home/Home.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
-import { Container, Grid, Grow } from "@material-ui/core";
+import { Container, Grid, Grow, Paper } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { getPosts } from "../../actions/posts";
+import Pagination from "../../compontents/Pagination/Pagination";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
+import useStyles from "./styles";
 
 export const Home = () => {
     const [currentId, setCurrentId] = useState(null);
     const dispatch = useDispatch();
+    const classes = useStyles();
 
     useEffect(() => {
         dispatch(getPosts());
@@ -21,6 +24,9 @@ export const Home = () => {
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Form currentId={ currentId } setCurrentId={ setCurrentId }/>
+                        <Paper className={classes.pagination} elevation={6}>
+                            <Pagination />
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
