0fa79755e7f9d6ecb763deadf1f905b9ce3c6bd9
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-16 02:02:48 -0600

    Redesign frontend layout and add styles

--- client/src/compontents/NavBar/NavBar.js
+++ client/src/compontents/NavBar/NavBar.js
@@ -4,7 +4,8 @@ import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { decode } from "jsonwebtoken";
 import useStyles from "./styles";
-import memory from "../../images/memory.png";
+import memoriesLogo from "../../images/memories-Logo.png";
+import memoriesText from "../../images/memories-Text.png";
 import { LOGOUT } from "../../constants/actionTypes";
 
 export const NavBar = () => {
@@ -31,10 +32,10 @@ export const NavBar = () => {
 
     return (
         <AppBar className={classes.appBar} position={"static"} color={"inherit"}>
-            <div className={classes.brandContainer}>
-                <Typography component={Link} to="/" className={classes.heading} variant={"h2"} align={"center"}>Memory</Typography>
-                <img className={classes.image} src={memory} alt={"memory"} height={"60"} />
-            </div>
+            <Link to="/" className={classes.brandContainer}>
+                <img src={memoriesText} alt="icon" height={"45px"}/>
+                <img className={classes.image} src={memoriesLogo} alt={"icon"} height={"40px"} />
+            </Link>
             <Toolbar className={classes.toolbar}>
                 { user ? (
                     <div className={classes.profile}>
