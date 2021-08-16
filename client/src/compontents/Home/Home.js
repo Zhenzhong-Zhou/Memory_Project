83d64216014e14edc23203a38de29c5617fec459
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-16 02:52:29 -0600

    Done pagination functionality, bug fixes: Pagination to PaginationItem, Waiting (TTFB) and 304 not modified issues

--- client/src/compontents/Home/Home.js
+++ client/src/compontents/Home/Home.js
@@ -24,10 +24,6 @@ export const Home = () => {
     const [search, setSearch] = useState("");
     const [tags, setTags] = useState([]);
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
     const searchPost = () => {
         // dispatch -> fetch search post
         if (search.trim() || tags) {
@@ -65,7 +61,7 @@ export const Home = () => {
                         </AppBar>
                         <Form currentId={ currentId } setCurrentId={ setCurrentId }/>
                         <Paper className={classes.pagination} elevation={6}>
-                            <Pagination />
+                            <Pagination page={page}/>
                         </Paper>
                     </Grid>
                 </Grid>
--- controllers/posts.js
+++ controllers/posts.js
@@ -2,10 +2,16 @@ import PostMessage from "../models/postMessage.js";
 import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
-    try {
-        const postMessage = await PostMessage.find();
+    const { page } = req.query;
 
-        res.status(200).json(postMessage);
+    try {
+        // Can change any number of posts in a page
+        const LIMIT = 8;
+        const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+        const total = await PostMessage.countDocuments({});
+        const posts = await PostMessage.find().sort({_id: -1}).limit(LIMIT).skip(startIndex);
+
+        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
