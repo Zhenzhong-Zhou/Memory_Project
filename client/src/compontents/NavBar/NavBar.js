41d47de4d7dfdf15b9a1e0773d0d30a72379e10e
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-16 23:33:10 -0600

    Reduce some warnings

--- client/src/compontents/NavBar/NavBar.js
+++ client/src/compontents/NavBar/NavBar.js
@@ -28,6 +28,7 @@ export const NavBar = () => {
             if (decodedToken.exp * 1000 < new Date().getTime()) logout();
         }
         setUser(JSON.parse(localStorage.getItem("profile")));
+        // eslint-disable-next-line
     }, [dispatch, location]);
 
     return (
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,5 +1,6 @@
 import { FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH, START_LOADING, END_LOADING, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
 
+// eslint-disable-next-line
 export default (state = { isLoading: true, posts: [] }, action) => {
     switch (action.type) {
         case START_LOADING:
