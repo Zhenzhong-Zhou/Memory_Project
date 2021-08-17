1877916341e46ac55a01f7a34568c6427d02a6e6
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-17 15:41:25 -0600

    Add comment section frontend functionality

--- client/src/compontents/PostDetails/PostDetails.js
+++ client/src/compontents/PostDetails/PostDetails.js
@@ -4,9 +4,10 @@ import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
 import { useParams, useHistory } from "react-router-dom";
 
-import emptyImage from "../../images/emptyImage.png"
+import CommentSection from "./CommentSection";
 import { getPost, getPostsBySearch } from "../../actions/posts";
 import useStyles from "./styles"
+import emptyImage from "../../images/emptyImage.png"
 
 const PostDetails = () => {
     const { post, posts, isLoading } = useSelector((state) => state.posts);
@@ -47,9 +48,9 @@ const PostDetails = () => {
                     <Typography variant="h6">Created by: {post.name}</Typography>
                     <Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
                     <Divider style={{ margin: '20px 0' }} />
-                    <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
+                    <Typography variant="body1"><strong>Realtime Chat</strong></Typography>
                     <Divider style={{ margin: '20px 0' }} />
-                    <Typography variant="body1"><strong>Comments - coming soon!</strong></Typography>
+                    <CommentSection post={post} />
                     <Divider style={{ margin: '20px 0' }} />
                 </div>
                 <div className={classes.imageSection}>
