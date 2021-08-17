df3d28f2988a2ad176e98447a2c696e68546a7b2
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-16 21:06:18 -0600

    Done recommended post

--- client/src/compontents/PostDetails/PostDetails.js
+++ client/src/compontents/PostDetails/PostDetails.js
@@ -5,7 +5,7 @@ import moment from "moment";
 import { useParams, useHistory } from "react-router-dom";
 
 import emptyImage from "../../images/emptyImage.png"
-import { getPost } from "../../actions/posts";
+import { getPost, getPostsBySearch } from "../../actions/posts";
 import useStyles from "./styles"
 
 const PostDetails = () => {
@@ -19,13 +19,23 @@ const PostDetails = () => {
        dispatch(getPost(id));
     }, [id]);
 
+    useEffect(() => {
+        if (post) {
+            dispatch(getPostsBySearch({ search: "none", tags: post?.tags.join(",") }))
+        }
+    }, [post]);
+
     if (!post) return null;
 
     if (isLoading) {
         return <Paper elevation={6} className={classes.loadingPaper}>
             <CircularProgress size={"7em"} />
         </Paper>
-    }
+    };
+
+    const recommendedPosts = posts.filter(({_id}) => _id !== post._id);
+
+    const openPost = (_id) => history.push(`/posts/${_id}`)
 
     return (
         <Paper style={{ padding: "20px", borderRadius: "15px" }} elevation={6}>
@@ -46,6 +56,23 @@ const PostDetails = () => {
                     <img className={classes.media} src={post.selectedFile || emptyImage} alt={post.title} />
                 </div>
             </div>
+            {recommendedPosts.length && (
+                <div className={classes.selection}>
+                    <Typography gutterBottom variant={"h5"}>You might also like:</Typography>
+                    <Divider />
+                    <div className={classes.recommendedPosts}>
+                        {recommendedPosts.map(({ title, message, name, likes, selectedFile, _id}) => (
+                            <div style={{margin: "20px", cursor: "pointer"}} onClick={() => openPost(_id)} key={_id}>
+                                <Typography gutterBottom variant={"h6"}>{title}</Typography>
+                                <Typography gutterBottom variant={"subtitle2"}>{name}</Typography>
+                                <Typography gutterBottom variant={"subtitle2"}>{message}</Typography>
+                                <Typography gutterBottom variant={"subtitle1"}>Likes: {likes.length}</Typography>
+                                <img src={selectedFile} width={"200px"} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </Paper>
     );
 };
